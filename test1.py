f3e1f2c5efba9a52c0127dc74bdc3a0420665f57
noobsahil <96654829+noobsahil@users.noreply.github.com>
2023-03-09 14:07:55 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+a = 33
+b = "33"
+
+if int(b) > a:
+    print("b is greater than a")
+elif a == int(b):
+    print("a and b are equal")
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
---
+++ test2.py
@@ -0,0 +1,14 @@
+a = 83
+b = "83"
+
+if int(b) > a:
+    print("b is greater than a")
+elif a == int(b):
+    print("a and b are equal")
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
